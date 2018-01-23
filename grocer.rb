9f2826be3a3d94e8eef385fc4e15217c2729da13
York Wang <yw544@nyu.edu>
2018-01-23 09:08:02 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
   # code here
+  hash = {}
+  cart.each do |i_hash|
+    item.each do |item, info|
+      if i_hash.include?(item)
+        hash[item][:count] += 1
+      else
+        hash[item] = info
+        hash[item][:count] = 1
+      end
+    end
+end
+hash
 end
 
 def apply_coupons(cart, coupons)
